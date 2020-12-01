38c062cc715fdc1af0b38b3812440cfb09f2e19f
Gil Heller <heller.gil@gmail.com>
2020-12-01 15:23:12 +0200

    detect - fix order of operations; edge - simplify detector

--- detect.py
+++ detect.py
@@ -56,8 +56,8 @@ def main():
     image = read_image(image_file)
 
     print("Detecting edges", end="")
-    # edges_image = detect_edges(image, threshold_val=100, blur_val=5)
-    edges_image = image
+    # According to clarifications - threshold for edge detection is 100
+    edges_image = detect_edges(image, threshold_val=100)
     print(" - Done!")
 
     print("Detecting lines", end="")
@@ -65,8 +65,7 @@ def main():
     print(" - Done!")
 
     print("Detecting circles", end="")
-    # circles = detect_circles(edges_image)
-    circles = []
+    circles = detect_circles(edges_image)
     print(" - Done!")
 
     image = cv.cvtColor(image, cv.COLOR_GRAY2RGB)
@@ -74,14 +73,14 @@ def main():
     draw_lines_on_img(lines, image)
     draw_circles_on_img(circles, image)
 
-    display_image(image)
-    save_image(image)
-
     write_log_file(lines, circles, output_file)
+
+    save_image(image)
+    display_image(image)
 
 
 if __name__ == "__main__":
-    argv.append("one_diagonal_line.png")
+    argv.append("many_lines.png")
     argv.append("output.txt")
     if len(argv) != 3:
         print("Usage: {0} [image] [output]".format(argv[0]))
--- edge.py
+++ edge.py
@@ -4,30 +4,37 @@ import numpy as np
 PADDING = 1
 
 
-def detect_edges(img, threshold_val, blur_val):
-    img = blur_image(img, blur_val)
+def detect_edges(img, threshold_val):
     img = add_padding_to_image(img)
     img = apply_edge_detection(img)
     apply_threshold(img, threshold_val)
-    img = apply_dilation(img)
-    img = apply_thinning(img)
     img = remove_padding_from_image(img)
 
     return img
 
 
-def blur_image(img, val=5):
-    return cv.GaussianBlur(img, (val, val), 0)
-
-
 def apply_edge_detection(img):
-    grad_x = cv.Sobel(img, cv.CV_16S, 1, 0, ksize=3, scale=1, delta=0, borderType=cv.BORDER_DEFAULT)
-    grad_y = cv.Sobel(img, cv.CV_16S, 0, 1, ksize=3, scale=1, delta=0, borderType=cv.BORDER_DEFAULT)
+    sobel_x_convoluted = np.float32([[-1, 0, 1],
+                                     [-2, 0, 2],
+                                     [-1, 0, 1]])
 
-    abs_grad_x = cv.convertScaleAbs(grad_x)
-    abs_grad_y = cv.convertScaleAbs(grad_y)
+    sobel_y_convoluted = np.float32([[-1, -2, -1],
+                          [0, 0, 0],
+                          [1, 2, 1]])
 
-    return cv.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+    height, width = img.shape
+    new_image = create_empty_img(height, width)
+
+    for y in range(PADDING, height - PADDING):
+        for x in range(PADDING, width - PADDING):
+            roi = img[y - 1: y + 2, x - 1: x + 2]
+            gx = np.dot(roi.flatten(), sobel_x_convoluted.flatten())
+            gy = np.dot(roi.flatten(), sobel_y_convoluted.flatten())
+            g = (gx ** 2 + gy ** 2) ** 0.5
+
+            new_image[y][x] = g
+
+    return new_image
 
 
 def add_padding_to_image(img, add_h=PADDING, add_w=PADDING):
@@ -94,49 +101,3 @@ def apply_threshold(img, val=127):
             else:
                 img[y][x] = 255
     return img
-
-
-def apply_dilation(img):
-    height, width = img.shape
-    new_image = create_empty_img(height, width)
-
-    for y in range(PADDING, height - PADDING):
-        for x in range(PADDING, width - PADDING):
-            # Get all the 9 pixels area around the current pixel
-            roi = img[y - 1: y + 2, x - 1: x + 2]
-
-            # if at least one pixel in the region is white, then the
-            # center pixel is white
-            if 255 in roi:
-                new_image[y][x] = 255
-            # if all other pixels are black - then the center will be black
-            else:
-                new_image[y][x] = 0
-
-    return new_image
-
-
-def apply_thinning(img):
-    # Structuring Element
-    kernel = cv.getStructuringElement(cv.MORPH_CROSS, (3, 3))
-    # Create an empty output image to hold values
-    thin = np.zeros(img.shape, dtype='uint8')
-
-    temp_image = img.copy()
-
-    # Loop until erosion leads to an empty set
-    while cv.countNonZero(temp_image) != 0:
-        # Erosion
-        erode = cv.erode(temp_image, kernel)
-        # Opening on eroded image
-        opening = cv.morphologyEx(erode, cv.MORPH_OPEN, kernel)
-        # Subtract these two
-        subset = erode - opening
-        # Union of all previous sets
-        thin = cv.bitwise_or(subset, thin)
-        # Set the eroded image for next iteration
-        temp_image = erode.copy()
-
-    return thin
-
-
