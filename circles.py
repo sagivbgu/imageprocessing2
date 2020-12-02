4808c8e3d4bb259f64de93d1a0cdfd5060019f91
sagivbgu <sagivsc@post.bgu.ac.il>
2020-12-02 14:31:03 +0200

    Circles: Changed to method of checking a, b, r. Extracted threshold to a constant.

--- circles.py
+++ circles.py
@@ -1,61 +1,46 @@
 import json
-from math import sqrt, floor, sin, cos, pi
+import math
 import cv2 as cv
 
 from utils import get_pixels_with_value
 
+THRESHOLD = 0.8  # Used to choose votes from the hough matrix.
+                 # If the amount of votes > the circle's perimeter * THRESHOLD, then draw this circle. Otherwise, assume
+                 # it's not a circle but noise and ignore this cell in the hough matrix.
+                 # In tests we've done, a value between 0.3~0.6 is optimal most of the times.
+MIN_RADIUS = 8  # Circles with radius less than MIN_RADIUS will be considered as noise and ignored.
 DETECTED_CIRCLE_COLOR = (0, 255, 255)
-
-"""
-Parametric equation of a circle:
-x = a + rcos(theta)
-y = b + rsin(theta)
-0 <= theta < 2 * pi
-"""
 
 
 class HoughMatrix3D:
     def __init__(self, img_height, img_width):
-        img_diagonal = sqrt(img_height ** 2 + img_width ** 2)
-        self._r_max = self._transform_r(img_diagonal)
-        self._a_max = self._transform_a(img_width)
-        self._b_max = self._transform_b(img_height)
-        self._theta_slices = 100
-        self._theta_quanta = 2 * pi / self._theta_slices
-        self._theta_max = self._transform_theta(2 * pi)
-        self._mat = [[[0 for _ in range(self._b_max)] for _ in range(self._a_max)] for _ in range(self._r_max)]
+        img_diagonal = math.sqrt(img_height ** 2 + img_width ** 2)
+        self._a_max = img_width
+        self._b_max = img_height
+        self._r_max = math.floor(img_diagonal)
+        self._mat = [[[0 for _ in range(self._r_max)] for _ in range(self._b_max)] for _ in range(self._a_max)]
 
     def increment_by_x_y(self, x, y):
-        for r_index in range(self._r_max):
-            r = self._inverse_r(r_index)
-            for theta_index in range(self._theta_max):
-                theta = self._inverse_theta(theta_index)
-                a = x - int(r * cos(theta))
-                b = y - int(r * sin(theta))
-                a_index = self._transform_a(a)
-                b_index = self._transform_b(b)
-                if 0 <= a_index < self._a_max and 0 <= b_index < self._b_max:
-                    self._mat[r_index][a_index][b_index] += 1
+        for a in range(self._a_max):
+            for b in range(self._b_max):
+                r = math.sqrt((x - a) ** 2 + (y - b) ** 2)
+                r = math.floor(r)
+                self._mat[a][b][r] += 1
 
     def get_all_above_threshold(self):
-        circles = []
-        for r_index in range(self._r_max):
-            r = self._inverse_r(r_index)
-            for a_index in range(self._a_max):
-                a = self._inverse_a(a_index)
-                for b_index in range(self._b_max):
-                    b = self._inverse_b(b_index)
-                    value = self._mat[r_index][a_index][b_index]
-                    if self._is_above_threshold(r, value):
-                        new_circle = (a, b, r)
-                        circles.append((new_circle, value))
-        return circles
+        return [  # List comprehension
+            ((a, b, r), self._mat[a][b][r])
+            for r in range(self._r_max)
+            for b in range(self._b_max)
+            for a in range(self._a_max)
+            if self._is_above_threshold(r, self._mat[a][b][r])
+        ]
 
     def _is_above_threshold(self, radius, cell_value):
-        if radius <= 8:
+        if radius <= MIN_RADIUS:  # Don't catch too small circles to reduce noise
             return False
-        threshold = 0.3  # Recommended so far: 0.3 ~ 0.33 / 0.45 ~ 0.6
-        return cell_value / self._theta_slices >= threshold
+        perimeter = 2 * math.pi * radius
+        return cell_value >= perimeter * THRESHOLD
 
     def dump(self, filename):
         with open(filename, "w") as f:
@@ -64,31 +49,6 @@ class HoughMatrix3D:
     def load(self, filename):
         with open(filename, "r") as f:
             self._mat = json.load(f)
-
-    def _transform_r(self, r):
-        return int(r)
-
-    def _transform_a(self, a):
-        return int(a)
-
-    def _transform_b(self, b):
-        return int(b)
-
-    def _transform_theta(self, theta):
-        new_theta = theta / self._theta_quanta
-        return floor(new_theta)
-
-    def _inverse_r(self, index):
-        return index
-
-    def _inverse_a(self, index):
-        return index
-
-    def _inverse_b(self, index):
-        return index
-
-    def _inverse_theta(self, index):
-        return index * self._theta_quanta
 
 
 def detect_circles(image, debug=False):  # TODO: Remove debug feature
@@ -104,7 +64,13 @@ def detect_circles(image, debug=False):  # TODO: Remove debug feature
         hough_matrix.dump("mat.json")
 
     circles = hough_matrix.get_all_above_threshold()
+    circles = [c for c in circles if is_circle_inside_image(c[0], h, w)]  # c[0] is the circle (a, b, r), c[1] is the value of the corresponding cell in the hough matrix
     return get_unique_circles(circles)
+
+
+def is_circle_inside_image(circle, img_height, img_width):
+    a, b, r = circle
+    return a + r < img_width and a - r >= 0 and b + r < img_height and b - r >= 0
 
 
 def get_unique_circles(circles):
@@ -118,11 +84,12 @@ def get_unique_circles(circles):
 
 
 def close_points(p1, p2):
+    # Points are considered close if the distance between their a, b and r values is <= 2 pixels
     return all(abs(p2[i] - p1[i]) <= 2 for i in range(3))
 
 
 def draw_circles_on_img(circles, img):
-    if len(circles) == 0:
+    if not len(circles):
         return
     for (a, b, r) in circles:
         cv.circle(img, (a, b), r, DETECTED_CIRCLE_COLOR, 1)
